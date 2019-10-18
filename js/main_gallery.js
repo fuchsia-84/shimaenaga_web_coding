6930865e415aee8fd4c8d2a6b802913ae918527c
fuchsia-84 <mikitech9084@gmail.com>
2019-10-19 00:10:50 +0900

    fix link, make flickity work ONLY for sp.

--- js/main_gallery.js
+++ js/main_gallery.js
@@ -1,8 +1,17 @@
-$(function() {
-    $('.main_gallery').flickity({
-        autoPlay: true,
-        draggble: true,
-        pageDots: true,
-        prevNextButtons: false,
-    });
+var $win = $(window);
+
+$win.on('load resize', function() {
+
+    var windowWidth = window.innerWidth;
+
+    if(windowWidth < 767) {
+        $('.main_gallery').flickity({
+                autoPlay: true,
+                draggble: true,
+                pageDots: true,
+                prevNextButtons: false,
+        })
+    } else {
+        $('.main_gallery').flickity('destroy')
+    }
 });
