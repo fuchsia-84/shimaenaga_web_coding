e7671fbe22e9f6ff284af846d4f582ae2ad32496
fuchsia-84 <mikitech9084@gmail.com>
2019-11-01 15:53:16 +0900

    fix flickity initialize error.

--- js/main_gallery.js
+++ js/main_gallery.js
@@ -1,17 +1,25 @@
 var $win = $(window);
+var isFlickity = true;
+var $main_gallery = $('.main_gallery').flickity();
 
 $win.on('load resize', function() {
 
     var windowWidth = window.innerWidth;
 
-    if(windowWidth < 767) {
-        $('.main_gallery').flickity({
+    if(windowWidth > 767) {
+        if(isFlickity) {
+            $main_gallery.flickity('destroy');
+            isFlickity = false;
+        }
+    } else {
+        if(!isFlickity) {
+            $main_gallery.flickity({
                 autoPlay: true,
                 draggble: true,
                 pageDots: true,
                 prevNextButtons: false,
-        })
-    } else {
-        $('.main_gallery').flickity('destroy')
+            });
+            isFlickity = true;
+        }
     }
 });
